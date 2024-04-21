cd139d7d123e3a7be55b6659f0adc106770aa845
Margarita <margarita.levitm@checkmarx.com>
2024-04-21 16:47:21 +0300

    Update BookDetail_jsp_new - Copy.java

--- BookDetail_jsp_new - Copy.java
+++ BookDetail_jsp_new - Copy.java
@@ -6,8 +6,8 @@ import org.apache.jasper.runtime.*;
 public class BookDetail_jsp extends HttpJspBase {
 
 
-//
-//   Filename: Common.jsp
+
+  Filename: Common.jsp
 //   Generated with CodeCharge  v.1.2.0
 //   JSP.ccp build 05/21/2001
 //
